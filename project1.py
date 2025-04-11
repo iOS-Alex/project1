cc9238d59b1b55e914e4acc47eb98e323af83a04
Alex Forrester <200848319+iOS-Alex@users.noreply.github.com>
2025-04-10 21:04:42 -0400

    commit

--- project1.py
+++ project1.py
@@ -1,8 +1,16 @@
 import statsapi
 
-sched = statsapi.schedule(start_date='04/08/2025',end_date='04/09/2025',team=143,opponent=121)
-team = statsapi.get('team', {'teamId':143})
+# get schedules for teams
+sched = statsapi.schedule(start_date='04/11/2025',end_date='04/11/2025',team="",opponent="")
 
+# get teams, how do i get a list of all teams and their ID's?
+team = statsapi.get('team', {'teamId':112})
+print(sched)
 
+# where do I store my data once I pull it?
 
-print('The A\'s won %s games in 2024.' % sum(1 for x in statsapi.schedule(team=133,start_date='01/01/2025',end_date='12/31/2025') if x.get('winning_team','')=='Oakland Athletics'))
+# why can't I see data for 2025?
+#print('The A\'s won %s games in 2018.' % sum(1 for x in statsapi.schedule(team=133,start_date='01/01/2018',end_date='12/31/2018') if x.get('winning_team','')=='Oakland Athletics'))
+
+# Dodgers ID 119
+# Cubs ID 112
